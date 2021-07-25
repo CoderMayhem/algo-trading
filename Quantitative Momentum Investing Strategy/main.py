0305d5fb0835c16dd653e09a14445665c1d3638e
CoderMayhem <namanmishra1900@gmail.com>
2021-07-26 00:21:29 +0530

    completed project 2

--- Quantitative Momentum Investing Strategy/main.py
+++ Quantitative Momentum Investing Strategy/main.py
@@ -1,5 +1,7 @@
+from os import write
 import numpy as np
 import pandas as pd
+from pandas.core.arrays import string_
 from pandas.io import api
 import requests
 import math
@@ -44,4 +46,160 @@ for symbol_string in symbol_strings[:1]:
             ignore_index=True
         )
 
-print(final_dataframe)
+#removing low momentum stocks from the data-frame
+#logic : sort the dataframe on the basis of 1Yr return and then take the top 50 stocks
+
+final_dataframe.sort_values('One-Year Price Return',ascending = False, inplace = True)
+final_dataframe = final_dataframe[:50]
+final_dataframe.reset_index(inplace = True)
+
+#calculating the number of shares to buy
+def portfolio_input():
+    global portfolio_size
+    portfolio_size = input('Enter the size of your portfolio...')
+    try:
+        float(portfolio_size)
+    except:
+        print('Enter the value of your portolio as an integer only...')
+        portfolio_size = input('Enter the size of your portfolio...')
+
+
+# for i in range(0,len(final_dataframe)):
+#     final_dataframe.loc[i,'Number of Shares to Buy'] = math.floor(position_size/final_dataframe.loc[i,'Price'])
+
+#building a better and more realistic quantitative momentum strategy
+hqm_columns = [
+    'Ticker',
+    'Price',
+    'Number of Shares to Buy',
+    'One-Year Price Return',
+    'One-Year Return Percentile',
+    'Six-Month Price Return',
+    'Six-Month Return Percentile',
+    'Three-Month Price Return',
+    'Three-Month Return Percentile',
+    'One-Month Price Return',
+    'One-Month Return Percentile',
+    'HQM Score'
+]
+
+hqm_dataframe = pd.DataFrame(columns=hqm_columns)
+
+for symbol_string in symbol_strings:
+    batch_api_call_url = f'https://sandbox.iexapis.com/stable/stock/market/batch/?types=stats,quote&symbols={symbol_string}&token={IEX_CLOUD_API_TOKEN}'
+    data = requests.get(batch_api_call_url).json()
+    for symbol in symbol_string.split(','):
+        hqm_dataframe = hqm_dataframe.append(
+            pd.Series(
+                [
+                    symbol,
+                    data[symbol]['quote']['latestPrice'],
+                    'N/A',
+                    data[symbol]['stats']['year1ChangePercent'],
+                    'N/A',
+                    data[symbol]['stats']['month6ChangePercent'],
+                    'N/A',
+                    data[symbol]['stats']['month3ChangePercent'],
+                    'N/A',
+                    data[symbol]['stats']['month1ChangePercent'],
+                    'N/A',
+                    'N/A'
+                ],
+                index=hqm_columns),
+                ignore_index=True
+        )
+
+#calculating momentum percentile
+
+time_periods = [
+    'One-Year',
+    'Six-Month',
+    'Three-Month',
+    'One-Month'
+]
+
+for row in hqm_dataframe.index:
+    for time_period in time_periods:
+        hqm_dataframe.loc[row, f'{time_period} Return Percentile'] = stats.percentileofscore(hqm_dataframe[f'{time_period} Price Return'], hqm_dataframe.loc[row, f'{time_period} Price Return'])/100
+    
+#calculating hqm score
+from statistics import mean
+
+for row in hqm_dataframe.index:
+    momentum_percentiles = []
+    for time_period in time_periods:
+        momentum_percentiles.append(hqm_dataframe.loc[row, f'{time_period} Return Percentile'])
+    hqm_dataframe.loc[row,'HQM Score'] = mean(momentum_percentiles)
+
+#Selecting the Top 50 Momentum Stocks based on HQM Score
+hqm_dataframe.sort_values('HQM Score', ascending=False, inplace=True)
+hqm_dataframe = hqm_dataframe[:50]
+hqm_dataframe.reset_index(drop= True, inplace = True)
+
+portfolio_input()
+position_size = float(portfolio_size)/len(hqm_dataframe.index)
+for i in hqm_dataframe.index:
+    hqm_dataframe.loc[i, 'Number of Shares to Buy'] = math.floor(position_size/hqm_dataframe.loc[i,'Price'])
+
+#formatting the excel output
+writer = pd.ExcelWriter('momentum_strategy.xlsx', engine='xlsxwriter')
+hqm_dataframe.to_excel(writer, sheet_name="Momentum Strategy", index = False)
+
+background_color = '#0a0a23'
+font_color = '#ffffff'
+
+string_template = writer.book.add_format(
+        {
+            'font_color': font_color,
+            'bg_color': background_color,
+            'border': 1
+        }
+    )
+
+dollar_template = writer.book.add_format(
+        {
+            'num_format':'$0.00',
+            'font_color': font_color,
+            'bg_color': background_color,
+            'border': 1
+        }
+    )
+
+integer_template = writer.book.add_format(
+        {
+            'num_format':'0',
+            'font_color': font_color,
+            'bg_color': background_color,
+            'border': 1
+        }
+    )
+
+percent_template = writer.book.add_format(
+        {
+            'num_format':'0.0%',
+            'font_color': font_color,
+            'bg_color': background_color,
+            'border': 1
+        }
+    )
+
+column_formats = { 
+                    'A': ['Ticker', string_template],
+                    'B': ['Price', dollar_template],
+                    'C': ['Number of Shares to Buy', integer_template],
+                    'D': ['One-Year Price Return', percent_template],
+                    'E': ['One-Year Return Percentile', percent_template],
+                    'F': ['Six-Month Price Return', percent_template],
+                    'G': ['Six-Month Return Percentile', percent_template],
+                    'H': ['Three-Month Price Return', percent_template],
+                    'I': ['Three-Month Return Percentile', percent_template],
+                    'J': ['One-Month Price Return', percent_template],
+                    'K': ['One-Month Return Percentile', percent_template],
+                    'L': ['HQM Score', integer_template]
+                }
+
+for column in column_formats.keys():
+    writer.sheets['Momentum Strategy'].set_column(f'{column}:{column}', 20, column_formats[column][1])
+    writer.sheets['Momentum Strategy'].write(f'{column}1', column_formats[column][0], string_template)
+
+writer.save()
