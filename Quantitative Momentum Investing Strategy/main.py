e1a7ac00f51cb998115ed5a8a5100f5cc863c9da
CoderMayhem <namanmishra1900@gmail.com>
2021-07-25 22:39:52 +0530

    project2 starter code

---
+++ Quantitative Momentum Investing Strategy/main.py
@@ -0,0 +1,47 @@
+import numpy as np
+import pandas as pd
+from pandas.io import api
+import requests
+import math
+from scipy import stats
+import xlsxwriter
+
+stocks = pd.read_csv('sp_500_stocks.csv')
+from secrets import IEX_CLOUD_API_TOKEN
+
+symbol = 'AAPL'
+api_url = f'https://sandbox.iexapis.com/stable/stock/{symbol}/stats/?token={IEX_CLOUD_API_TOKEN}'
+data = requests.get(api_url).json()
+
+data['year1ChangePercent']
+
+def chunks(lst,n):
+    for i in range(0,len(lst),n):
+        yield lst[i:i+n]
+
+symbol_groups = list(chunks(stocks['Ticker'],100))
+symbol_strings = []
+for i in range(0,len(symbol_groups)):
+    symbol_strings.append(','.join(symbol_groups[i]))
+
+my_columns = ['Ticker', 'Price', 'One-Year Price Return', 'Number of Shares to Buy']
+
+final_dataframe = pd.DataFrame(columns=my_columns)
+for symbol_string in symbol_strings[:1]:
+    batch_api_call_url = f'https://sandbox.iexapis.com/stable/stock/market/batch/?types=stats,quote&symbols={symbol_string}&token={IEX_CLOUD_API_TOKEN}'
+    data = requests.get(batch_api_call_url).json()
+    for symbol in symbol_string.split(','):
+        final_dataframe = final_dataframe.append(
+            pd.Series(
+                [
+                    symbol,
+                    data[symbol]['quote']['latestPrice'],
+                    data[symbol]['stats']['year1ChangePercent'],
+                    'N/A'
+                ],
+                index=my_columns
+            ),
+            ignore_index=True
+        )
+
+print(final_dataframe)
